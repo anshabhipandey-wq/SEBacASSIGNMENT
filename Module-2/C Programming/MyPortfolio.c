30916b7bfe021c2ae712ae2018debf90f98a75a9
Ansh <anshabhipandey@gmail.com>
2026-08-11 22:10:01 +0530

    third commit

---
+++ Module-2/C Programming/MyPortfolio.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main(){
+	printf("========================================================\n");
+	printf("                 My Portfolio                            \n");
+	printf("========================================================\n");
+	
+	printf("Name			: Abhishek Pandey\n");
+	printf("Age			: 21\n");
+	printf("DOB 			: 10/05/2005\n");
+	printf("Father Name		: Mr. Anil Kumar Pandey\n");
+	printf("Mother Name		: Mintu Pandey\n");
+	printf("Education		: BCA\n");
+	printf("Skills			: C,C++,HTML,CSS,Python\n");
+	printf("Contact No.		: 8874831025\n");
+	printf("Email Id		: anshabhipandey@gmail.com\n");
+	printf("Address			: Pandesara,Surat\n");
+	printf("Hobbies			: Coding,Riding,Playing Cricket\n");
+	printf("========================================================\n");
+	
+	return 0;
+}
